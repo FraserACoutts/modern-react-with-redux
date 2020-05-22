f33dbe734263b446bc7d017664d969cb6bf0ec33
Fraser Coutts <fcouttsdev@gmail.com>
2020-05-22 18:05:39 +0100

    installing axios and looking at how to use the unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID JW9GW63-xjFAe-gB-7rgc9Ty4nBrpN5WGVKTCNk4JvE'
+            }
+        });
     }
 
     render() {
