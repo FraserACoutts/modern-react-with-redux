df5c7a4127ec02ee2bd690226c5c1c6cb1134993
Fraser Coutts <fcouttsdev@gmail.com>
2020-06-29 16:22:29 +0100

    Taking a refresh looking at communicating with props

--- components/src/index.js
+++ components/src/index.js
@@ -1,46 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard>
-                <div>
-                    <h4>Warning!</h4>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
                 </div>
-                Are you sure you want to do this?
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    timeAgo="Today at 4:45PM"
-                    content="Made breakfast"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Alex"
-                    timeAgo="Today at 2:00AM"
-                    content="Can't sleep..."
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail
-                    author="Jane"
-                    timeAgo="Yesterday at 5:00PM"
-                    content="Had a great time at the gym!!"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-
+            </div>
         </div>
     );
 };
