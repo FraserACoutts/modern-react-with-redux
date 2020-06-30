64c2e82d93db2c729291817f5578fe6acce7ba11
Fraser Coutts <fcouttsdev@gmail.com>
2020-06-30 15:40:56 +0100

    A refresh on state in components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
 
 class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        );
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-
-        return <Spinner message="Please accept location request" />;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
     }
 
-    render() {
-        return <div>{this.renderContent()}</div>;
-    }
+    return <div>Loading!</div>;
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />, document.querySelector('#root')
 );
