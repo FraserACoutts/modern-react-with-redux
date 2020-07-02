eb7fe8d1a2b87ce2dd9c80d885faac627091c236
Fraser Coutts <fcouttsdev@gmail.com>
2020-07-02 17:01:31 +0100

    Getting a list of data to render to screen.  Using key props and why they are used

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import axios from 'axios';
 
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './imageList';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID 34d6a9a63a5a929186629acb5c30ec1c2f7d564e428ae77da9a8db97b596e0a6'
-      }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
     });
 
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
