342d5e8d356a51cd72b204e024d0cee34eb831a0
Fraser Coutts <fcouttsdev@gmail.com>
2020-07-02 14:05:35 +0100

    Using input fields to get data to an api

--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID JW9GW63-xjFAe-gB-7rgc9Ty4nBrpN5WGVKTCNk4JvE'
-    }
-});
