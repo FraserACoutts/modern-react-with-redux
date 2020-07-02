b7a01e838c8b28f15316fc530fc15f2721ffe1d9
Fraser Coutts <fcouttsdev@gmail.com>
2020-07-02 16:07:56 +0100

    The differences between axios and fetch. Using async await to get back a response from an api call

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 34d6a9a63a5a929186629acb5c30ec1c2f7d564e428ae77da9a8db97b596e0a6'
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
